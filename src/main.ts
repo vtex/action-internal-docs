1d708e7475b3b07d86fd396e665a18c1ffaf7750
João Marcos Gris <joao.gris@vtex.com.br>
2021-03-04 14:13:34 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import {debug, setFailed} from '@actions/core'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    debug('s')
+    const x = github.context.payload.action
+
+    debug(x || '')
   } catch (error) {
     setFailed(error.message)
   }

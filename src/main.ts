50275f075e0412d7ee262982679282bd1455cb3b
João Marcos Gris <joao.gris@vtex.com.br>
2021-03-04 14:17:43 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import * as github from '@actions/github'
 async function run(): Promise<void> {
   try {
     const x = github.context.payload.action
-
+    // eslint-disable-next-line no-console
+    console.log(x)
     debug(x || '')
   } catch (error) {
     setFailed(error.message)

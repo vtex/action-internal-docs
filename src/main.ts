ace9a40c91708705c81e019339554f6990a17a59
João Marcos Gris <joao.gris@vtex.com.br>
2021-03-04 13:46:55 -0300

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
 import * as core from '@actions/core'
-import axios from 'axios'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-    const data = (await axios.get('https://example.org/')).data
-    core.debug(data)
-    console.log(data)
-    core.setOutput('time', new Date().toTimeString())
+    core.debug('s')
   } catch (error) {
     core.setFailed(error.message)
   }

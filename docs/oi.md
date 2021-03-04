6d6c2c0dd8a09e99ab7076587c35a31de8bfbd12
João Marcos Gris <joao.gris@vtex.com.br>
2021-03-04 16:38:50 -0300

    update

---
+++ docs/oi.md
@@ -0,0 +1,2 @@
+dssd
+fsdfsd
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,17 @@
 import {setFailed, getInput} from '@actions/core'
 import * as github from '@actions/github'
-import * as exec from '@actions/exec'
+import * as fs from 'fs-extra'
 
 async function run(): Promise<void> {
   try {
     // const x = github.context.payload
     const x = getInput('repo-token')
-    await exec.exec('ls')
-    await exec.exec('tree')
     // eslint-disable-next-line no-console
     console.log(x)
     const ctx = github.context
+    const fileList = await fs.readdir('/docs')
 
     const client = github.getOctokit(getInput('repo-token'))
-    // eslint-disable-next-line no-console
-    console.log(JSON.stringify(ctx.repo))
-    // eslint-disable-next-line no-console
-    console.log(JSON.stringify(ctx.eventName))
-    const ref = await client.git.getRef({
-      owner: ctx.repo.owner,
-      repo: ctx.repo.repo,
-      ref: 'heads/main'
-    })
 
     const repo = await client.git.getRef({
       owner: ctx.repo.owner,
@@ -30,7 +20,7 @@ async function run(): Promise<void> {
     })
 
     // eslint-disable-next-line no-console
-    console.log(JSON.stringify(ref))
+    console.log(JSON.stringify(fileList))
     // eslint-disable-next-line no-console
     console.log(JSON.stringify(repo))
   } catch (error) {

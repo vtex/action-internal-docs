c5aed17fb7d6efda4f86f14859f38f5f93a610bf
João Marcos Gris <joao.gris@vtex.com.br>
2021-03-04 14:34:47 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import {setFailed} from '@actions/core'
+import {setFailed, getInput} from '@actions/core'
 import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const x = github.context.payload
+    // const x = github.context.payload
+
+    const client = github.getOctokit(getInput('repo-token', {required: true}))
+    const pulls = await client.pulls.list()
+
     // eslint-disable-next-line no-console
-    console.log(JSON.stringify(x))
+    console.log(JSON.stringify(pulls))
   } catch (error) {
     setFailed(error.message)
   }

afe79ea47be67827c6fc35fda60edd253827d3f6
Lucas Cordeiro <ecdb.lucas@gmail.com>
2022-06-23 14:51:36 -0300

    Fix syntax for if

--- tests/check-no-pr-open.sh
+++ tests/check-no-pr-open.sh
@@ -5,7 +5,7 @@ pr_number=$1
 if [ ! -z "$pr_number" ]; then
   echo "Expected to not have opened a pull-request, but PR #$pr_number was opened"
 
-  if [ -! -z "$CI" ]; then
+  if [ ! -z "$CI" ]; then
     gh pr close $pr_number --delete-branch
   fi
 
